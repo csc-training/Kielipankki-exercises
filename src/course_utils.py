2592ea5bdca1a9711d052325c92ae62878ad1f25
Sam Hardwick <sam.hardwick@csc.fi>
2023-06-08 15:08:08 +0300

    move more of the messy details from the Notebook here

--- src/course_utils.py
+++ src/course_utils.py
@@ -1,9 +1,17 @@
 import os
 
 data_dir = os.path.abspath(os.getcwd() + "/../../")
-
+data_dir = "/home/shardwic/github/nlp-tools/vrt/"
 
 import datetime
 
-def increment_month(date):
-    return (date.replace(day=1) + datetime.timedelta(days=40)).replace(day=1)
+def month_range(start_date, end_date):
+    '''Given start_date and end_date (of type datetime.date), return
+    a list of dates representing the first days of every month overlapping
+    their range.'''
+    months = []
+    month = start_date.replace(day=1)
+    while month <= end_date:
+        months.append(month)
+        month = (month + datetime.timedelta(days=40)).replace(day=1)
+    return months
